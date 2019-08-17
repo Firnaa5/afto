ca282c5292eb9d900a2db82dbbce7b2aa27e6508
Firnaa5 <46587472+Firnaa5@users.noreply.github.com>
2019-08-17 12:50:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyAOEXtFfROFentchCyCKzd8mLduhrEZ7Hk'
+});
+const port = 3000;
+
+//Middleware for cors
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.send(`The Afto Map Api is running @ ${port}`);
+})
+
+app.listen(port, function (req, res) {
+    console.log(`Server is running @ ${port}`);
+})
+
+//API call for distance
+app.get('/distance', mapDistance);
+
+function mapDistance (req, res) {
+    const options = {
+        origin: req.query.origin,
+        destination: req.query.destination
+    }
+    googleMapsClient.directions(options, function (err, response) {
+        if (!err) {
+            console.log(response);
+            res.json(response.json.routes[0].legs)
+        }
+    });
+}
+
